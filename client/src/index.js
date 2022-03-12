c2077d669d3e8cf769191f4b798b363aa1c68722
kalkidan12 <kalget12@gmail.com>
2022-03-12 13:48:13 +0300

    index chnage

--- client/src/index.js
+++ client/src/index.js
@@ -3,16 +3,15 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.min.js';
+
 import {BrowserRouter as Router } from 'react-router-dom';
 
 
 ReactDOM.render(
   <React.StrictMode>
-    
-      <Router>
+       <Router>
         <App />
       </Router>
-    
   </React.StrictMode>,
   document.getElementById('root')
 );
